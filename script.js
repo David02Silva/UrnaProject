113129fb49f6ff935871b2ccd64e3c770e08003a
David Silva <david02alexandre@gmail.com>
2023-10-16 01:12:45 +0100

    script developed, correcting some errors on the button (correct) on the screen--change

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let closeInfo = function(){
+let squList = [];
+let applyList = [];
+let squareNumbers = "";
+let screenHNumber = document.querySelectorAll(".screen--home .number")
+let screenHome = document.querySelector(".screen--home");
+let screenVote = document.querySelector(".screen--vote");
+let notFound = false;
+let keys = document.querySelectorAll(".keys .key");
+let keyVote = screenVote.querySelectorAll(".number")
+let voteOn = false;
+let chosen = "";
+
+
+
+let notFoundMens = document.createElement("h3");
+notFoundMens.innerHTML = "Party as been not found..."
+
+//Function for the "X" on the info to close it self
+function closeInfo(){
     document.querySelector(".party--stage").innerHTML = "";
     document.querySelector(".party--stage").style.display = "none"
     document.querySelector(".applicants--container").style.display = "flex";
-}
+};
 
+//Applicant info filled with all information from our Json 
 appList.forEach((item)=>{
     let applicant = document.querySelector(".model .applicant-info").cloneNode(true);
 
@@ -42,6 +61,152 @@ appList.forEach((item)=>{
     })
 })
 
+//Filling our candidate lists (squList and applyList) with the values from our Json
+for(i in appList){
+
+    squList.push(appList[i].number)
+
+    appList[i].applicant.forEach((app)=>{
+        applyList.push(parseInt(app.number))
+   })
+
+}
 
 
 
+// Showing the Keys on the Home Squares
+keys.forEach((key)=>{
+    key.addEventListener("click", ()=>{
+        if(screenHome.style.display != "none"){
+            squareNumbers += key.innerHTML
+
+            if(squareNumbers.length == 1){
+                screenHNumber[0].classList.remove("selected")
+                screenHNumber[1].classList.add("selected")
+            }
+
+            if(squareNumbers.length > 2){
+                squareNumbers = "";
+                squareNumbers += key.innerHTML
+                screenHNumber[1].classList.add("selected")
+            }
+            
+        screenHNumber[0].innerHTML = squareNumbers[0];
+
+        if(squareNumbers.length == 2){
+            screenHNumber[1].classList.remove("selected")
+            screenHNumber[1].innerHTML = squareNumbers[1];
+        }else{
+            screenHNumber[1].innerHTML = "";
+        }
+        if(notFound == true){
+            notFound = false;
+            screenHome.removeChild(notFoundMens)
+        }
+        }
+
+        if(voteOn == true){
+            if(squareNumbers.length <= 3){  
+            squareNumbers += key.innerHTML;
+
+            keyVote.forEach((item, itemIndex, array) =>{
+                if(squareNumbers.length > itemIndex){
+                    item.innerHTML = squareNumbers[itemIndex]
+                    if(itemIndex < array.length - 1){
+                        item.classList.remove("selected")
+                        array[itemIndex + 1].classList.add("selected");
+                    }
+                    if(itemIndex == array.length - 1){
+                        item.classList.remove("selected")
+                    }
+                }
+            })
+                
+            }
+            
+
+        }
+
+        
+
+    })
+})
+
+
+// Button Enter
+document.querySelector(".btn--enter").addEventListener("click", ()=>{
+   if(squList.findIndex((squ) => squ === parseInt(squareNumbers)) == -1){
+    
+    if(notFound == true){
+        return
+    }else{
+        screenHome.appendChild(notFoundMens);
+        notFound = true;
+    }
+
+    }else{
+        if(voteOn == false){
+            screenHome.style.display = "none"
+            screenVote.style.display = "flex";
+            
+            chosen =  appList.find((item)=> item.number == parseInt(squareNumbers));
+            squareNumbers = "";
+        
+            
+            screenVote.querySelector(".partie h2 span").innerHTML = chosen.number;
+            voteOn = true;
+            console.log("Vote HOME")
+            return
+        }
+   }
+        if(voteOn == true){
+            console.log("VoteOOONNN")
+            if(chosen.applicant.findIndex((item)=> item.number == squareNumbers) === -1){
+                console.log("Não existe!")
+            }else{
+                console.log("Existe!!")
+            }
+            console.log(squareNumbers)
+        }
+})
+
+// Button Correct 
+document.querySelector(".btn--correct").addEventListener("click", ()=>{
+    if(voteOn == false){
+        squareNumbers = squareNumbers.slice(0, squareNumbers.length - 1);
+    
+        if(squareNumbers[0] == undefined){
+            screenHNumber[0].innerHTML = ""
+        }else{
+            screenHNumber[0].innerHTML = squareNumbers[0];
+        }
+    
+        if(squareNumbers[1] == undefined){
+            screenHNumber[1].innerHTML = "";
+        }else{
+            screenHNumber[1].innerHTML = squareNumbers[1];
+        }
+    } else {
+        keyVote.forEach((item, index, array) =>{
+                if(squareNumbers.length == index && squareNumbers.length > 0){
+                    array[squareNumbers.length - 1].innerHTML = "";
+                    array[squareNumbers.length - 1].classList.add("selected");
+                    if(squareNumbers.length < array.length){
+                        array[squareNumbers.length].classList.remove("selected")
+                    }
+                    squareNumbers = squareNumbers.slice(0, squareNumbers.length - 1);
+            }
+        })
+        if(squareNumbers.length == 4){
+            squareNumbers = squareNumbers.slice(0, squareNumbers.length - 1);
+            keyVote[3].innerHTML = "";
+            keyVote[3].classList.add("selected");
+        }
+        if(squareNumbers.length == 0){
+            console.log("Vai voltar!")
+        }
+
+    }
+    
+    
+})
